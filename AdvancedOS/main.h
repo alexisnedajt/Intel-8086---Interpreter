d9ca8ce6488ddfcc39dae8dabc6454103c84b380
alexisnedajt <alexisnedjat@gmail.com>
2024-06-28 00:34:49 +0900

    cleaning up

--- AdvancedOS/main.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int loop(char* memory, const char* s, int size, int index);
-
-int operations(char* memory, const char* s, int size, int* index);
-
-void interpreter(char* memory, const char* s);
